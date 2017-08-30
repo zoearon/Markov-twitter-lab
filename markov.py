4bd17d68b27be080577ad6d5334be18bb01da9bc
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-30 11:56:07 -0700

    tweet function implemented and capitalization fixed

--- markov.py
+++ markov.py
@@ -44,15 +44,26 @@ def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
     key = choice(chains.keys())
+    while True:
+        if key[0] != key[0].capitalize():
+            key = choice(chains.keys())
+        else:
+            break
+    #key = choice(chains.keys())
     words = [key[0], key[1]]
+    counter = len(key[0] + key[1]) + 1
+    # length of the first words and space
+
     while key in chains:
         # Keep looping until we have a key that isn't in the chains
         # (which would mean it was the end of our original text)
         #
         # Note that for long texts (like a full book), this might mean
         # it would run for a very long time.
-
         word = choice(chains[key])
+        counter = counter + 1 + len(word)
+        if counter > 140:
+            break
         words.append(word)
         key = (key[1], word)
 
@@ -63,7 +74,17 @@ def tweet(chains):
     # Use Python os.environ to get at environmental variables
     # Note: you must run `source secrets.sh` before running this file
     # to make sure these environmental variables are set.
-    pass
+    api = twitter.Api(
+        consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+        consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+        access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+        access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+    print api.VerifyCredentials()
+
+    status = api.PostUpdate(make_text(chains))
+
+    print status.text
 
 # Get the filenames from the user through a command line prompt, ex:
 # python markov.py green-eggs.txt shakespeare.txt
@@ -76,4 +97,14 @@ text = open_and_read_file(filenames)
 chains = make_chains(text)
 
 # Your task is to write a new function tweet, that will take chains as input
-# tweet(chains)
+#tweet(chains)
+print make_text(chains)
+
+# # Open the file and turn it into one long string
+# input_text = open_and_read_file(filenames)
+
+# # Get a Markov chain
+# chains = make_chains(input_text)
+
+# # Produce random text
+# random_text = make_text(chains)
